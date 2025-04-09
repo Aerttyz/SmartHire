2e08f88324c41fc58477514a3a63e150a9ba246d
Aerttyz <alesandrosilva2004@gmail.com>
2025-04-09 13:26:11 -0300

    feat: add PDF text extraction utility functions

---
+++ api/app/utils/extract_text_from_pdf.py
@@ -0,0 +1,16 @@
+import fitz # PyMuPDF
+from pathlib import Path
+
+def extract_text(path_pdf):
+    pdf = fitz.open(path_pdf)
+    text = ""
+    for page in pdf:
+        text += page.get_text()
+    return text
+
+def open_folder(path):
+    texts = []
+    for path_pdf in path.glob("*.pdf"):
+        text = extract_text(path_pdf)
+        texts.append(text)
+    return texts
