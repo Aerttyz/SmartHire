f6c735f0153a92ef7534d01a000b75e81365981f
Sávio Emanuel <130401186+savioemanuelf@users.noreply.github.com>
2025-04-17 11:23:21 -0300

    feat: implement exception handling

--- backend/src/main/java/com/smarthire/resume/services/ResumeService.java
+++ backend/src/main/java/com/smarthire/resume/services/ResumeService.java
@@ -1,10 +1,15 @@
 package com.smarthire.resume.services;
 
+import com.smarthire.resume.exceptions.EmptyPathException;
+import com.smarthire.resume.exceptions.FlaskServiceException;
+import com.smarthire.resume.exceptions.InvalidPathException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -16,14 +21,24 @@ public class ResumeService {
     private final String flaskUrl = "http://localhost:5000/extract_entities";
 
     public Map<String, Object> analyzeFolder(String path) {
+        if(path == null || path.isEmpty()) {
+            throw new EmptyPathException();
+        }
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> payload = Map.of("path", path);
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(payload, headers);
 
-        ResponseEntity<Map> response = restTemplate.postForEntity(flaskUrl, entity, Map.class);
-        return response.getBody();
+        try {
+            ResponseEntity<Map> response = restTemplate.postForEntity(flaskUrl, entity, Map.class);
+            return response.getBody();
+        } catch (HttpClientErrorException ex) {
+            throw new InvalidPathException();
+        } catch (ResourceAccessException ex) {
+            throw new FlaskServiceException();
+        }
     }
 }
 
