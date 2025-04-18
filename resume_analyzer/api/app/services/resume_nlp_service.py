98cb9e1580fe8e5d6d4edeaf7a6014393b9336b6
Aerttyz <alesandrosilva2004@gmail.com>
2025-04-18 02:23:26 -0300

    feat: integrate Gemini response into extract_entities output

--- resume_analyzer/api/app/services/resume_nlp_service.py
+++ resume_analyzer/api/app/services/resume_nlp_service.py
@@ -1,16 +1,25 @@
 import spacy
 from app.utils import extract_text_from_pdf
 from pathlib import Path
+from app.utils.generate_llm_prompts import generate_gemini_prompt
+from app.services import gemini_api_service
 
 def extract_entities(path):
     texts = extract_text_from_pdf.open_folder(path)
     model_path = Path("/app/nlp_models/output/model-best")
     nlp = spacy.load(model_path)
 
-    all_entities = []
-    for text in texts:
+    all_resumes = {}
+    for  i, text in enumerate(texts):
         doc = nlp(text)
         entities = [(ent.text, ent.label_) for ent in doc.ents]
-        all_entities.append(entities)
-    return all_entities    
+        clean_text = extract_text_from_pdf.clean_text(text, entities)
+        prompt = generate_gemini_prompt(clean_text)
+        response = gemini_api_service.gemini_response(prompt)
+        all_resumes[i] = {
+            "entities": entities,
+            "response": response.text
+        }
+        print(response)
+    return all_resumes    
     
